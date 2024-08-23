617f44d270e0933234cb799a1507bf8f0325c913
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-08-23 22:16:30 +0530

    Deep Clone polyfill

--- inteview-questions/js/prototype/prototype.js
+++ inteview-questions/js/prototype/prototype.js
@@ -1,1 +1,32 @@
 // 1. Deep Clone an object in JS?
+var obj1 = {
+  a: 1,
+  b: {
+    c: [1, 2],
+    d: {
+      e: 3,
+    },
+  },
+};
+
+function deepClone(obj) {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+
+  let clone = Array.isArray(obj) ? [] : {};
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clone[key] = deepClone(obj[key]);
+    }
+  }
+  return clone;
+}
+
+const clonedObj = deepClone(obj1);
+clonedObj.b.d.e = 5;
+
+console.log(clonedObj);
+
+console.log(obj1);
